268725ee8e66a6b98141019c6885bcc478ccbefc
jersilb1400 <jersilb@gracefm.org>
2025-06-05 20:33:21 -0500

    Updated the frontend logo and dependencies

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Card sx={{ maxWidth: 480, width: '100%', borderRadius: 3, boxShadow: 3 }}>
         <CardContent sx={{ textAlign: 'center', p: 5 }}>
           {/* Optional: Add your logo here */}
-          {<img src="/public/gracefm.png" alt="Logo" style={{ width: 80, marginBottom: 24 }} /> }
+          {<img src="/gracefm.png" alt="Logo" style={{ width: 80, marginBottom: 24 }} /> }
           <Typography variant="h4" gutterBottom sx={{ fontWeight: 700 }}>
             Welcome to Congregational Care
           </Typography>
